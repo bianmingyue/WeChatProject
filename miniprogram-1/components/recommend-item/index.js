02d3b67cb486f339bd46910784cf7adc86099365
bianmy <bianmingyue0121@gmail.com>
2023-03-23 20:52:37 +0800

    weChatDemo

---
+++ miniprogram-1/components/recommend-item/index.js
@@ -0,0 +1,31 @@
+// components/recommend-item/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+     title:{
+       value:"标题"
+     },
+     titleBoxColor:{
+       value:"#7a9ff8"
+     },
+     titleColor:{
+      value:"#7a9ff8"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
